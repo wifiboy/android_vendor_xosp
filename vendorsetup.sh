bd7697ede5d9a42ec4fc365f80556f74fd440f5b
Anthony King <anthonydking@slimroms.net>
2015-05-01 00:00:37 +0100

    vendor: reborn: change how device combos are added
    device names are now added to reborn.devices.
    
    A custom device list file can be used by calling:
      reborn_device_combos "path/custom.devices"
    
    A custom variant can be used by calling:
      reborn_device_combos "eng"
    
    Both can be used by calling:
      reborn_device_combos "path/custom.devices" "eng"
    
    setting either of these overwrites the default values
    
    This also introduces the start of our vendor envsetup
    Change-Id: I12ddf088a1377233640c359dc2f4fb305018052e

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,3 @@
-for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-13.0 | awk '{printf "cm_%s-%s\n", $1, $2}')
-do
-    add_lunch_combo $combo
-done
+. "$(gettop)/vendor/reborn/build/envsetup.sh"
+
+reborn_device_combos
