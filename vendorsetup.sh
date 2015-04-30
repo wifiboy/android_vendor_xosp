7be8136e822d9b4ffdf78b405fc52bc78cfc6bb9
Anthony King <anthonydking@slimroms.net>
2015-05-01 00:00:37 +0100

    vendor: XOSP: change how device combos are added
    device names are now added to xosp.devices
    
    A custom device list file can be used by calling:
      xosp_device_combos "path/custom.devices"
    
    A custom variant can be used by calling:
      xosp_device_combos "eng"
    
    Both can be used by calling:
      xosp_device_combos "path/custom.devices" "eng"
    
    setting either of these overwrites the default values

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,3 @@
-for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-14.0 | awk '{printf "cm_%s-%s\n", $1, $2}')
-do
-    add_lunch_combo $combo
-done
+. "$(gettop)/vendor/xosp/build/xospDevices.sh"
+
+xosp_device_combos
