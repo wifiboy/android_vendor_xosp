98030297d2290c1e28af1d8125e31ddace7e47fe
nilac8991 <nilac8991@gmail.com>
2015-12-07 13:13:20 +0100

    Add Xperia Calendar

--- config/reborn_apps.mk
+++ config/reborn_apps.mk
@@ -4,6 +4,7 @@
 PRODUCT_COPY_FILES += \
     vendor/reborn/prebuilt/common/apps/ClockWidgets.apk:system/app/ClockWidgets/ClockWidgets.apk \
 		vendor/reborn/prebuilt/common/apps/Home.apk:system/app/ClockWidgets/Home.apk \
-		vendor/reborn/prebuilt/common/apps/WeatherWidget.apk:system/app/ClockWidgets/WeatherWidget.apk
-
+		vendor/reborn/prebuilt/common/apps/WeatherWidget.apk:system/app/ClockWidgets/WeatherWidget.apk \
+		vendor/reborn/prebuilt/common/apps/SemcClock.apk:system/app/SemcClock/SemcClock.apk
+		
 $(call inherit-product, vendor/reborn/config/reborn_extras.mk)
