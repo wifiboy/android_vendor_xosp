0091568461b67d574acc819e6522e53eed70c449
nilac8991 <nilac8991@gmail.com>
2015-12-08 21:43:39 +0100

    Remove deprecated apps

--- config/reborn_apps.mk
+++ config/reborn_apps.mk
@@ -2,10 +2,8 @@
 # Copy S+ Reborn Apps
 
 PRODUCT_COPY_FILES += \
-    vendor/reborn/prebuilt/common/apps/ClockWidgets.apk:system/app/ClockWidgets/ClockWidgets.apk \
-		vendor/reborn/prebuilt/common/apps/Home.apk:system/app/ClockWidgets/Home.apk \
-		vendor/reborn/prebuilt/common/apps/WeatherWidget.apk:system/app/ClockWidgets/WeatherWidget.apk \
+		vendor/reborn/prebuilt/common/apps/Home.apk:system/app/Home/Home.apk \
 		vendor/reborn/prebuilt/common/apps/SemcClock.apk:system/app/SemcClock/SemcClock.apk \
 		vendor/reborn/prebuilt/common/apps/WallpaperPicker.apk:system/app/WallpaperPicker/WallpaperPicker.apk
-		
+
 $(call inherit-product, vendor/reborn/config/reborn_extras.mk)
