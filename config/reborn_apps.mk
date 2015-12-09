d8a66db24f3e025f62c2349e282561078cc4cd48
nilac8991 <nilac8991@gmail.com>
2015-12-09 21:32:13 +0100

    Squashed update for Xperia Apps

--- config/reborn_apps.mk
+++ config/reborn_apps.mk
@@ -4,6 +4,11 @@
 PRODUCT_COPY_FILES += \
 		vendor/reborn/prebuilt/common/apps/Home.apk:system/app/Home/Home.apk \
 		vendor/reborn/prebuilt/common/apps/SemcClock.apk:system/app/SemcClock/SemcClock.apk \
-		vendor/reborn/prebuilt/common/apps/WallpaperPicker.apk:system/app/WallpaperPicker/WallpaperPicker.apk
+		vendor/reborn/prebuilt/common/apps/WallpaperPicker.apk:system/app/WallpaperPicker/WallpaperPicker.apk \
+		vendor/reborn/prebuilt/common/apps/ClockWidget.apk:system/app/ClockWidget/ClockWidget.apk \
+		vendor/reborn/prebuilt/common/apps/Music.apk:system/priv-app/Music/Music.apk \
+		vendor/reborn/prebuilt/common/apps/Sketch.apk:system/app/Sketch/Sketch.apk \
+		vendor/reborn/prebuilt/common/apps/Video.apk:system/priv-app/Video/Video.apk \
+		vendor/reborn/prebuilt/common/apps/WeatherWidget.apk:system/app/WeatherWidget/WeatherWidget.apk
 
 $(call inherit-product, vendor/reborn/config/reborn_extras.mk)
