46f57d9876d7f53a6b657b0b8965b52a1305009f
Calin <nilac8991@gmail.com>
2015-12-31 16:00:49 +0100

    Add PhotoEditor
    - Moved also everything which is not refered to Apps to Reborn Extra like libs and so on
    Signed-off-by: Calin <nilac8991@gmail.com>

--- config/reborn_apps.mk
+++ config/reborn_apps.mk
@@ -1,7 +1,7 @@
 
 # Copy S+ Reborn Apps
 
-# Normal Apps
+# Xperia Apps
 
 PRODUCT_COPY_FILES += \
 		vendor/reborn/prebuilt/common/apps/Home.apk:system/app/Home/Home.apk \
@@ -16,34 +16,8 @@ PRODUCT_COPY_FILES += \
 		vendor/reborn/prebuilt/common/apps/SmartConnect.apk:system/app/SmartConnect/SmartConnect.apk \
 		vendor/reborn/prebuilt/common/apps/AudioRecorder.apk:system/app/AudioRecorder/AudioRecorder.apk \
 		vendor/reborn/prebuilt/common/apps/SemCalendar.apk:system/priv-app/SemCalendar/SemCalendar.apk \
-		vendor/reborn/prebuilt/common/frameworks/com.sonyericsson.uxpres.jar:system/frameworks/com.sonyericsson.uxpres.jar \
-		vendor/reborn/prebuilt/common/frameworks/com.sonymobile.provider.tasks.jar:system/frameworks/com.sonymobile.provider.tasks.jar \
-		vendor/reborn/prebuilt/common/frameworks/SemcGenericUxpRes.apk:system/frameworks/SemcGenericUxpRes.apk \
-		vendor/reborn/prebuilt/common/etc/permissions/com.sonyericsson.uxpres.xml:system/etc/permissions/com.sonyericsson.uxpres.xml \
-		vendor/reborn/prebuilt/common/etc/permissions/com.sonymobile.provider.tasks.xml:system/etc/permissions/com.sonymobile.provider.tasks.xml \
 		vendor/reborn/prebuilt/common/apps//textinput-tng.apk:system/priv-app/textinput-tng/textinput-tng.apk \
-		vendor/reborn/prebuilt/common/lib/libswiftkeysdk-java.so:system/priv-app/textinput-tng/lib/arm/libswiftkeysdk-java.so \
 		vendor/reborn/prebuilt/common/apps/Album.apk:system/priv-app/Album/Album.apk \
-		vendor/reborn/prebuilt/common/lib/libdrmstream_depjb.so:system/priv-app/Album/lib/arm/libdrmstream_depjb.so \
-		vendor/reborn/prebuilt/common/lib/libgifreader.so:system/priv-app/Album/lib/arm/libgifreader.so \
-		vendor/reborn/prebuilt/common/lib/libimagecalc.so:system/priv-app/Album/lib/arm/libimagecalc.so \
-		vendor/reborn/prebuilt/common/lib/libjpeguser.so:system/priv-app/Album/lib/arm/libjpeguser.so \
-		vendor/reborn/prebuilt/common/lib/libmaketc.so:system/priv-app/Album/lib/arm/libmaketc.so \
-		vendor/reborn/prebuilt/common/lib/libnativeio.so:system/priv-app/Album/lib/arm/libnativeio.so \
-		vendor/reborn/prebuilt/common/lib/libsceniccore.so:system/priv-app/Album/lib/arm/libsceniccore.so \
-		vendor/reborn/prebuilt/common/lib/libspf.so:system/priv-app/Album/lib/arm/libspf.so \
-		vendor/reborn/prebuilt/common/lib/libSQHashGenerator.so:system/priv-app/Album/lib/arm/libSQHashGenerator.so
-
-# Custom Xperia Apps (Propietary frameworks and libs required)
-
-# Semc Calendar 
-
-
-# textinput-tng
-
-
-
-# Album
-
+		vendor/reborn/prebuilt/common/apps/PhotoEditor.apk:system/priv-app/PhotoEditor/PhotoEditor.apk
 		
 $(call inherit-product, vendor/reborn/config/reborn_extras.mk)
