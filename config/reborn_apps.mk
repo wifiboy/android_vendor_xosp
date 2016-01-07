538614c8b96d3c0b82f780759b5794f52757536a
Calin <nilac8991@gmail.com>
2016-01-07 15:53:08 +0100

    Revert "Add PhotoEditor"
    This reverts commit 46f57d9876d7f53a6b657b0b8965b52a1305009f.

--- config/reborn_apps.mk
+++ config/reborn_apps.mk
@@ -1,7 +1,7 @@
 
 # Copy S+ Reborn Apps
 
-# Xperia Apps
+# Normal Apps
 
 PRODUCT_COPY_FILES += \
 		vendor/reborn/prebuilt/common/apps/Home.apk:system/app/Home/Home.apk \
@@ -16,8 +16,34 @@ PRODUCT_COPY_FILES += \
 		vendor/reborn/prebuilt/common/apps/SmartConnect.apk:system/app/SmartConnect/SmartConnect.apk \
 		vendor/reborn/prebuilt/common/apps/AudioRecorder.apk:system/app/AudioRecorder/AudioRecorder.apk \
 		vendor/reborn/prebuilt/common/apps/SemCalendar.apk:system/priv-app/SemCalendar/SemCalendar.apk \
+		vendor/reborn/prebuilt/common/frameworks/com.sonyericsson.uxpres.jar:system/framework/com.sonyericsson.uxpres.jar \
+		vendor/reborn/prebuilt/common/frameworks/com.sonymobile.provider.tasks.jar:system/framework/com.sonymobile.provider.tasks.jar \
+		vendor/reborn/prebuilt/common/frameworks/SemcGenericUxpRes.apk:system/framework/SemcGenericUxpRes.apk \
+		vendor/reborn/prebuilt/common/etc/permissions/com.sonyericsson.uxpres.xml:system/etc/permissions/com.sonyericsson.uxpres.xml \
+		vendor/reborn/prebuilt/common/etc/permissions/com.sonymobile.provider.tasks.xml:system/etc/permissions/com.sonymobile.provider.tasks.xml \
 		vendor/reborn/prebuilt/common/apps//textinput-tng.apk:system/priv-app/textinput-tng/textinput-tng.apk \
+		vendor/reborn/prebuilt/common/lib/libswiftkeysdk-java.so:system/priv-app/textinput-tng/lib/arm/libswiftkeysdk-java.so \
 		vendor/reborn/prebuilt/common/apps/Album.apk:system/priv-app/Album/Album.apk \
-		vendor/reborn/prebuilt/common/apps/PhotoEditor.apk:system/priv-app/PhotoEditor/PhotoEditor.apk
+		vendor/reborn/prebuilt/common/lib/libdrmstream_depjb.so:system/priv-app/Album/lib/arm/libdrmstream_depjb.so \
+		vendor/reborn/prebuilt/common/lib/libgifreader.so:system/priv-app/Album/lib/arm/libgifreader.so \
+		vendor/reborn/prebuilt/common/lib/libimagecalc.so:system/priv-app/Album/lib/arm/libimagecalc.so \
+		vendor/reborn/prebuilt/common/lib/libjpeguser.so:system/priv-app/Album/lib/arm/libjpeguser.so \
+		vendor/reborn/prebuilt/common/lib/libmaketc.so:system/priv-app/Album/lib/arm/libmaketc.so \
+		vendor/reborn/prebuilt/common/lib/libnativeio.so:system/priv-app/Album/lib/arm/libnativeio.so \
+		vendor/reborn/prebuilt/common/lib/libsceniccore.so:system/priv-app/Album/lib/arm/libsceniccore.so \
+		vendor/reborn/prebuilt/common/lib/libspf.so:system/priv-app/Album/lib/arm/libspf.so \
+		vendor/reborn/prebuilt/common/lib/libSQHashGenerator.so:system/priv-app/Album/lib/arm/libSQHashGenerator.so
+
+# Custom Xperia Apps (Propietary frameworks and libs required)
+
+# Semc Calendar 
+
+
+# textinput-tng
+
+
+
+# Album
+
 		
 $(call inherit-product, vendor/reborn/config/reborn_extras.mk)
--- config/reborn_extras.mk
+++ config/reborn_extras.mk
@@ -3,8 +3,6 @@
 WALLPAPERS_VENDOR_PATH := vendor/reborn/prebuilt/common/etc/customization/content/com/sonyericsson/wallpaperpicker/wallpapers
 WALLPAPERS_SYSTEM_PATH := system/etc/customization/content/com/sonyericsson/wallpaperpicker/wallpapers
 
-# Xperia Wallpapers
-
 PRODUCT_COPY_FILES += \
 		$(WALLPAPERS_VENDOR_PATH)/A1.png:$(WALLPAPERS_SYSTEM_PATH)/A1.png \
 		$(WALLPAPERS_VENDOR_PATH)/A1_small.png:$(WALLPAPERS_SYSTEM_PATH)/A1_small.png \
@@ -58,39 +56,3 @@ PRODUCT_COPY_FILES += \
 		$(WALLPAPERS_VENDOR_PATH)/Dark_small.png:$(WALLPAPERS_SYSTEM_PATH)/Dark_small.png \
 		$(WALLPAPERS_VENDOR_PATH)/Light.png:$(WALLPAPERS_SYSTEM_PATH)/Light.png \
 		$(WALLPAPERS_VENDOR_PATH)/Light_small.png:$(WALLPAPERS_SYSTEM_PATH)/Light_small.png
-
-# Lib
-
-PRODUCT_COPY_FILES += \
-		vendor/reborn/prebuilt/common/lib/libdrmstream_depjb.so:system/priv-app/Album/lib/arm/libdrmstream_depjb.so \
-		vendor/reborn/prebuilt/common/lib/libgifreader.so:system/priv-app/Album/lib/arm/libgifreader.so \
-		vendor/reborn/prebuilt/common/lib/libimagecalc.so:system/priv-app/Album/lib/arm/libimagecalc.so \
-		vendor/reborn/prebuilt/common/lib/libjpeguser.so:system/priv-app/Album/lib/arm/libjpeguser.so \
-		vendor/reborn/prebuilt/common/lib/libmaketc.so:system/priv-app/Album/lib/arm/libmaketc.so \
-		vendor/reborn/prebuilt/common/lib/libnativeio.so:system/priv-app/Album/lib/arm/libnativeio.so \
-		vendor/reborn/prebuilt/common/lib/libsceniccore.so:system/priv-app/Album/lib/arm/libsceniccore.so \
-		vendor/reborn/prebuilt/common/lib/libspf.so:system/priv-app/Album/lib/arm/libspf.so \
-		vendor/reborn/prebuilt/common/lib/libSQHashGenerator.so:system/priv-app/Album/lib/arm/libSQHashGenerator.so \
-		vendor/reborn/prebuilt/common/lib/libswiftkeysdk-java.so:system/priv-app/textinput-tng/lib/arm/libswiftkeysdk-java.so \
-		vendor/reborn/prebuilt/common/lib/libjni_eglfence.so:system/priv-app/PhotoEditor/lib/arm/libjni_eglfence.so \
-		vendor/reborn/prebuilt/common/lib/libjni_filtershow_filters.so:system/priv-app/PhotoEditor/lib/arm/libjni_filtershow_filters.so \
-		vendor/reborn/prebuilt/common/lib/librs.convolve3x3.so:system/priv-app/PhotoEditor/lib/arm/librs.convolve3x3.so \
-		vendor/reborn/prebuilt/common/lib/librs.grad.so:system/priv-app/PhotoEditor/lib/arm/librs.grad.so \
-		vendor/reborn/prebuilt/common/lib/librs.grey.so:system/priv-app/PhotoEditor/lib/arm/librs.grey.so \
-		vendor/reborn/prebuilt/common/lib/librs.saturation.so:system/priv-app/PhotoEditor/lib/arm/librs.saturation.so \
-		vendor/reborn/prebuilt/common/lib/librs.vignette.so:system/priv-app/PhotoEditor/lib/arm/librs.vignette.so \
-		vendor/reborn/prebuilt/common/lib/librsjni.so:system/priv-app/PhotoEditor/lib/arm/librsjni.so \
-		vendor/reborn/prebuilt/common/lib/libRSSupport.so:system/priv-app/PhotoEditor/lib/arm/libRSSupport.so 
-
-# Frameworks
-
-PRODUCT_COPY_FILES += \
-		vendor/reborn/prebuilt/common/frameworks/com.sonyericsson.uxpres.jar:system/framework/com.sonyericsson.uxpres.jar \
-		vendor/reborn/prebuilt/common/frameworks/com.sonymobile.provider.tasks.jar:system/framework/com.sonymobile.provider.tasks.jar \
-		vendor/reborn/prebuilt/common/frameworks/SemcGenericUxpRes.apk:system/framework/SemcGenericUxpRes.apk
-
-# Permissions
-
-PRODUCT_COPY_FILES += \
-		vendor/reborn/prebuilt/common/etc/permissions/com.sonyericsson.uxpres.xml:system/etc/permissions/com.sonyericsson.uxpres.xml \
-		vendor/reborn/prebuilt/common/etc/permissions/com.sonymobile.provider.tasks.xml:system/etc/permissions/com.sonymobile.provider.tasks.xml 
