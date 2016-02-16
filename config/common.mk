3b68eabe87d0793afb566d0aace8a6cf06fc38f8
Scott Mertz <scott@cyngn.com>
2016-02-16 09:09:43 -0800

    Revert "recovery: default recovery updating to false"
    The default behavior is as if this setting is off.  There's no need
    to set it.  Settings will now set it at boot if not configured.
    
    This reverts commit e0e8c2772ec580d5a69adf742ede09f438b18399.
    
    OPO-490
    Change-Id: I77963dfc9eea8ece25ed1b6b17bd2c6936ae69ee

--- config/common.mk
+++ config/common.mk
@@ -265,9 +265,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 XOSP_DISPLAY_VERSION := $(XOSP_VERSION)
 
-# by default, do not update the recovery with system updates
-PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
-
 ifndef CM_PLATFORM_SDK_VERSION
   # This is the canonical definition of the SDK version, which defines
   # the set of APIs and functionality available in the platform.  It
