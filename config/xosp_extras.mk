1902c5accfce44899f0fa08b909f5b869b9df57f
Calin <nilac8991@gmail.com>
2016-04-08 13:40:20 +0200

    Forward port of Xperia Lockscreen Prototype (2/2)
    - Redesigned keyguard_status_area and keyguard_status_view almost from scratch
    - New Styles for the whole keyguard view
    - Added Xperia fonts needed by the Keyguard clock
    - XML sources are taken from decompiled SystemUI of Xperia Z5 compact with some little visual modifications by me for XOSP
    
    Credits goes also to idid idamrep for some other layout sources
    
    Signed-off-by: Calin <nilac8991@gmail.com>

--- config/xosp_extras.mk
+++ config/xosp_extras.mk
@@ -2,7 +2,10 @@
 
 WALLPAPERS_VENDOR_PATH := vendor/xosp/prebuilt/common/etc/customization/content/com/sonyericsson/wallpaperpicker/wallpapers
 WALLPAPERS_SYSTEM_PATH := system/etc/customization/content/com/sonyericsson/wallpaperpicker/wallpapers
+FONTS_VENDOR_PATH := vendor/xosp/prebuilt/common/fonts
+FONTS_SYSTEM_PATH := system/fonts
 
+#Wallpapers
 PRODUCT_COPY_FILES += \
 		$(WALLPAPERS_VENDOR_PATH)/A1.png:$(WALLPAPERS_SYSTEM_PATH)/A1.png \
 		$(WALLPAPERS_VENDOR_PATH)/A1_small.png:$(WALLPAPERS_SYSTEM_PATH)/A1_small.png \
@@ -20,3 +23,27 @@ PRODUCT_COPY_FILES += \
 		$(WALLPAPERS_VENDOR_PATH)/G1_small.png:$(WALLPAPERS_SYSTEM_PATH)/G1_small.png \
 		$(WALLPAPERS_VENDOR_PATH)/H1.png:$(WALLPAPERS_SYSTEM_PATH)/H1.png \
 		$(WALLPAPERS_VENDOR_PATH)/H1_small.png:$(WALLPAPERS_SYSTEM_PATH)/H1_small.png
+
+#Fonts
+PRODUCT_COPY_FILES += \
+		$(FONTS_VENDOR_PATH)/SST-UltraLight.ttf:$(FONTS_SYSTEM_PATH)/SST-UltraLight.ttf \
+		$(FONTS_VENDOR_PATH)/.SST-Condensed.ttf:$(FONTS_SYSTEM_PATH)/.SST-Condensed.ttf \
+		$(FONTS_VENDOR_PATH)/.SST-CondensedBd.ttf:$(FONTS_SYSTEM_PATH)/.SST-CondensedBd.ttf \
+		$(FONTS_VENDOR_PATH)/.SST-Heavy.ttf:$(FONTS_SYSTEM_PATH)/.SST-Heavy.ttf \
+		$(FONTS_VENDOR_PATH)/.SST-HeavyItalic.ttf:$(FONTS_SYSTEM_PATH)/.SST-HeavyItalic.ttf \
+		$(FONTS_VENDOR_PATH)/.SST-Light.ttf:$(FONTS_SYSTEM_PATH)/.SST-Light.ttf \
+		$(FONTS_VENDOR_PATH)/.SST-LightItalic.ttf:$(FONTS_SYSTEM_PATH)/.SST-LightItalic.ttf \
+		$(FONTS_VENDOR_PATH)/.SST-Medium.ttf:$(FONTS_SYSTEM_PATH)/.SST-Medium.ttf \
+		$(FONTS_VENDOR_PATH)/.SST-MediumItalic.ttf:$(FONTS_SYSTEM_PATH)/.SST-MediumItalic.ttf \
+		$(FONTS_VENDOR_PATH)/.SST-UltraLight.ttf:$(FONTS_SYSTEM_PATH)/.SST-UltraLight.ttf \
+		$(FONTS_VENDOR_PATH)/.SST-UltraLightItalic.ttf:$(FONTS_SYSTEM_PATH)/.SST-UltraLightItalic.ttf \
+		$(FONTS_VENDOR_PATH)/.SSTVietnamese-Bold.ttf:$(FONTS_SYSTEM_PATH)/.SSTVietnamese-Bold.ttf \
+		$(FONTS_VENDOR_PATH)/.SSTVietnamese-Roman.ttf:$(FONTS_SYSTEM_PATH)/.SSTVietnamese-Roman.ttf
+
+
+
+
+
+
+
+
