01689d49c9d5446ac6e8e335bb1aadadb18ce793
Calin <nilac8991@gmail.com>
2016-05-09 12:25:28 +0200

    Fix sepolicy paths
    Signed-off-by: Calin <nilac8991@gmail.com>

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,7 +1,7 @@
 #
 # This policy configuration will be used by all products that
-# inherit from S+ Reborn
+# inherit from XOSP
 #
 
 BOARD_SEPOLICY_DIRS += \
-    vendor/reborn/sepolicy
+    vendor/xosp/sepolicy
